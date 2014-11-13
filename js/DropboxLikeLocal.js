02e159b36c513936fb7db53388435155471feecc
topahl <pahlings@gmail.com>
2014-11-13 14:18:24 +0100

    Fixing error in dropboxDB creation
    Change-Id: If31c2153bc1ae95e3b798b7515b7f1e562eff8c6
    Signed-off-by: topahl <pahlings@gmail.com>

--- js/DropboxLikeLocal.js
+++ js/DropboxLikeLocal.js
@@ -1,108 +1,113 @@
 
-
 !(function (_global, undefined){
-  function switchManager(){
-    function DropboxDB(app_key) {
+  function dropboxDB(app_key) {
 
-      var ds = null
+    var ds = null
 
-      client.getDatastoreManager().openDefaultDatastore(function (error, datastore) {
-        if (error) {
-          error('Cannot open  default datastore, got error: ' + error);
-        } else {
-          ds = datastore;
-        }
-      });
-      function drop() {}
-
-      function tableCount() {
-        var list = ds.listTableIds()
-        return list.length;
-      };
-
-      function tableFields(table_name) {}
-
-      function tableExists(table_name) {}
-
-      function tableExistsWarn(table_name) {}
-
-      function columnExists(table_name, field_name) {}
-
-      function createTable(table_name, fields) {
-        // Tables are created on the fly
+    client.getDatastoreManager().openDefaultDatastore(function (error, datastore) {
+      if (error) {
+        error('Cannot open  default datastore, got error: ' + error);
+      } else {
+        ds = datastore;
       }
+    });
+    function drop() {}
 
-      function dropTable(table_name) {
-        //to delete a Dropbox table all enties must be deleted
-        var entries = ds.query();
-        for (key in item){
-          item.deleteRecord();
-        }
-      }
+    function tableCount() {
+      var list = ds.listTableIds()
+      return list.length;
+    };
 
-      function truncate(table_name) {}
+    function tableFields(table_name) {}
 
-      function alterTable(table_name, new_fields, default_values){}
+    function tableExists(table_name) {}
 
-      function rowCount(table_name) {
-        var table = ds.getTable(table_name);
-        var items = table.query();
-        return items.length;
-      }
+    function tableExistsWarn(table_name) {}
 
-      function insert(table_name, data) {
-        var table = ds.getTable(table_name);
-        table.insert(data);
-      }
+    function columnExists(table_name, field_name) {}
 
-      function select(table_name, ids, start, limit, sort) {}
-
-      function sort_results(field, order) {}
-
-      function queryByValues(table_name, data) {}
-
-      function queryByFunction(table_name, query_function) {}
-
-      function getIDs(table_name) {}
-
-      function deleteRows(table_name, ids) {}
-
-      function update(table_name, ids, update_function) {}
-
-      function commit() {}
-
-      function serialize() {}
-
-      function validateName(name) {}
-
-      function validFields(table_name, data) {}
-
-      function error(msg) {
-        throw new Error(msg);
-      }
-
-      function validateData(table_name, data) {}
-      return {
-         commit: function(){
-           return commit();
-         },
-         isNew: function(){
-           return db_new;
-         },
-         tableCount: function(){
-           return tableCount();
-         },
-         dropTable: function(table_name){
-           dropTable(table_name);
-         },
-         rowCount: function(table_name){
-           return rowCount(table_name);
-         },
-         insert: function(table_name, data){
-           insert(table_name,data);
-         }
-      }
-
+    function createTable(table_name, fields) {
+      // Tables are created on the fly
     }
+
+    function dropTable(table_name) {
+      //to delete a Dropbox table all enties must be deleted
+      var entries = ds.query();
+      for (key in item){
+        item.deleteRecord();
+      }
+    }
+
+    function truncate(table_name) {}
+
+    function alterTable(table_name, new_fields, default_values){}
+
+    function rowCount(table_name) {
+      var table = ds.getTable(table_name);
+      var items = table.query();
+      return items.length;
+    }
+
+    function insert(table_name, data) {
+      var table = ds.getTable(table_name);
+      table.insert(data);
+    }
+
+    function select(table_name, ids, start, limit, sort) {}
+
+    function sort_results(field, order) {}
+
+    function queryByValues(table_name, data) {}
+
+    function queryByFunction(table_name, query_function) {}
+
+    function getIDs(table_name) {}
+
+    function deleteRows(table_name, ids) {}
+
+    function update(table_name, ids, update_function) {}
+
+    function commit() {}
+
+    function serialize() {}
+
+    function validateName(name) {}
+
+    function validFields(table_name, data) {}
+
+    function error(msg) {
+      throw new Error(msg);
+    }
+
+    function validateData(table_name, data) {}
+    return {
+       commit: function(){
+         return commit();
+       },
+       isNew: function(){
+         return db_new;
+       },
+       tableCount: function(){
+         return tableCount();
+       },
+       dropTable: function(table_name){
+         dropTable(table_name);
+       },
+       rowCount: function(table_name){
+         return rowCount(table_name);
+       },
+       insert: function(table_name, data){
+         insert(table_name,data);
+       }
+    }
+
   }
+  // make amd compatible
+	if(typeof define === 'function' && define.amd) {
+		define(function() {
+			return dropboxDB;
+		});
+	} else {
+		_global['dropboxDB'] = dropboxDB;
+	}
 }(window));
