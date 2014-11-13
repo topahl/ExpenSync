bd01bbf40033114d68fd2c60c1fa3d9b842f569d
topahl <pahlings@gmail.com>
2014-11-13 21:59:44 +0100

    Basic Functionality enabled
    Change-Id: Idc6f210c7083a70ec18b18feceb67ed0d33eab1a
    Signed-off-by: topahl <pahlings@gmail.com>

--- js/DropboxLikeLocal.js
+++ js/DropboxLikeLocal.js
@@ -2,12 +2,15 @@
 !(function (_global, undefined){
   function dropboxDB(app_key) {
 
+    var META_TABLE = '_dbtables';
+
     var ds = null
+    var client = null
     setupDropbox();
 
 
     function setupDropbox(){
-      var client = new Dropbox.Client({key:app_key});
+      client = new Dropbox.Client({key:app_key});
       client.authenticate(null, function(error) {
       if(error)
           error("Dropbox authentication error:" + error);
@@ -42,14 +45,26 @@
     function columnExists(table_name, field_name) {}
 
     function createTable(table_name, fields) {
-      // Tables are created on the fly
+      var table = ds.getTable(META_TABLE);
+      if(tableExists(table_name))
+        error("Table "+table_name+" already excists");
+      table.insert({name:table_name});
+    }
+
+    function tableExists(table_name){
+      var table = ds.getTable(META_TABLE);
+      var result = table.query({name:table_name});
+      if(result.length > 0)
+        return true;
+      return false;
     }
 
     function dropTable(table_name) {
       //to delete a Dropbox table all enties must be deleted
-      var entries = ds.query();
-      for (key in item){
-        item.deleteRecord();
+      var table = ds.getTable(table_name);
+      var entries = table.query();
+      for (key in entries){
+        entires[key].deleteRecord();
       }
     }
 
@@ -72,9 +87,28 @@
 
     function sort_results(field, order) {}
 
-    function queryByValues(table_name, data) {}
+    function queryByValues(table_name, data) {
+      var table = ds.getTable(table_name);
+      var items = table.query(data);
+      var result = [];
+      for(key in items){
+        result.push(items[key].getFields());
+      }
+      return result;
+    }
 
-    function queryByFunction(table_name, query_function) {}
+    function queryByFunction(table_name, query_function) {
+      var table = ds.getTable(table_name);
+      var items = table.query();
+      var result = [];
+      for(key in items){
+        var item = items[key].getFields();
+        if(query_function(clone(item))==true){
+          result.push(item);
+        }
+      }
+      return result;
+    }
 
     function getIDs(table_name) {}
 
@@ -86,6 +120,16 @@
 
     function serialize() {}
 
+    function clone(obj){
+      var new_obj = {};
+			for(var key in obj) {
+				if( obj.hasOwnProperty(key) ) {
+					new_obj[key] = obj[key];
+				}
+			}
+			return new_obj;
+    }
+
     function validateName(name) {}
 
     function validFields(table_name, data) {}
@@ -94,13 +138,19 @@
       throw new Error(msg);
     }
 
+    function limit(array, start, limit){
+      if(typeof start == 'undefined' && typeof limit == 'undefined')
+        return array;
+      if(typeof start == 'undefined')
+        return array.slice(0,limit);
+      return array.slice(start,start+limit);
+    }
+
     function validateData(table_name, data) {}
+
     return {
-       commit: function(){
-         return commit();
-       },
-       isNew: function(){
-         return db_new;
+       createTable: function(table_name, fields){
+         createTable(table_name, fields);
        },
        tableCount: function(){
          return tableCount();
@@ -113,7 +163,18 @@
        },
        insert: function(table_name, data){
          insert(table_name,data);
+       },
+       query: function(table_name, query, limits, start, sort){
+         if(!query){
+           return limit(queryByValues(table_name),start,limits);
+         }
+         if(typeof query == 'function'){
+           return limit(queryByFunction(table_name, query),start,limits);
+         }
+         if(typeof query == 'object')
+         return limit(queryByFunction(table_name, query),start,limits);
        }
+
     }
 
   }
